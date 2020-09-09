3de2870f8578f227347634901a7995dd81c750df
Jordan Alba <jordan008@live.ca>
2020-09-09 14:12:37 -0400

    Anime added

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,9 +7,12 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             string myName = "Jordan Albayrak";
+            string myAnime = "Dora The Explorer";
             Console.WriteLine("Hello World");
             Console.ReadLine();
             Console.WriteLine("This is a test, my name is {0}", myName);
+            Console.ReadLine();
+            Console.WriteLine(" Top 1 greatest anime :  {0}", myAnime);
             Console.ReadLine();
         }
     }
