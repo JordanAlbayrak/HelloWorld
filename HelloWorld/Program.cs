8a5770a27c58a32591d67662334ac2fab20b3534
Jordan Alba <jordan008@live.ca>
2020-09-09 13:16:45 -0400

    A simple test, for a class porject. Added a Write line.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,10 +6,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            string myName = "Jordan";
             Console.WriteLine("Hello World");
             Console.ReadLine();
-
-
+            Console.WriteLine("This is a test, my name is {0}", myName);
+            Console.ReadLine();
         }
     }
 }
